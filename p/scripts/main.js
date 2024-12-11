12c659fb2fa21342d76b1a76c8c921117b9a48e5
Frans de Jonge <fransdejonge@gmail.com>
2024-12-11 14:47:01 +0100

    Always prevent default regardless of window.open() return status (#7089)
    * Always prevent default regardless of window.open() return status
    
    Fixes regression noted in https://github.com/FreshRSS/FreshRSS/pull/7077#discussion_r1879016226
    
    * Update p/scripts/main.js
    
    Co-authored-by: Alexandre Alapetite <alexandre@alapetite.fr>
    
    ---------
    
    Co-authored-by: Alexandre Alapetite <alexandre@alapetite.fr>

--- p/scripts/main.js
+++ p/scripts/main.js
@@ -1099,10 +1099,8 @@ function init_shortcuts() {
 
 			const link_go_website = document.querySelector('.flux.current a.go_website');
 			if (link_go_website) {
-				const newWindow = window.open(link_go_website.href, '_blank', 'noopener');
-				if (newWindow) {
-					ev.preventDefault();
-				}
+				window.open(link_go_website.href, '_blank', 'noopener');
+				ev.preventDefault();
 			}
 			return;
 		}
